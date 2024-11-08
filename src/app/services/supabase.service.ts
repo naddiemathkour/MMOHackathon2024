6be9b9adff874671425f824563452ae3da6bb434
naddiemathkour <naddiemathkour@gmail.com>
2024-11-08 16:53:09 -0500

    Updated to include Supabase connection

---
+++ src/app/services/supabase.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { SupabaseEnv } from '../env/environment';
+import { SupabaseClient, createClient } from '@supabase/supabase-js'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  constructor() {
+    this.supabase = createClient(SupabaseEnv.API_URL, SupabaseEnv.API_KEY);
+  }
+}
